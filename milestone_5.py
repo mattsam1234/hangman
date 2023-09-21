df8ccc70af74f46eba77f56a64d3c4a46acec4ae
Mattie Malling <mattsam1234@gmail.com>
2023-09-21 17:48:31 +0100

    added docstrings to the functions

--- milestone_5.py
+++ milestone_5.py
@@ -1,6 +1,8 @@
 import random
 
 class Hangman:
+    '''The Hangman class contains all the logic needed to play a game of hangman. When initialised it needs a list of words and optionally an intiger for the number of lives the player has
+    The ask_for_input method can be called to request the user input a letter and will then validate the letter and check if it is in the word.'''
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
@@ -12,7 +14,8 @@ class Hangman:
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
     
-    def check_guess(self, guess):
+    def __check_guess(self, guess):
+        '''This function checks is the guess is in the global scope variable word and then updates the word_guessed list with the letter at all positions it appears if the guess is correct'''
         guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -27,6 +30,7 @@ class Hangman:
             print(f"You have {self.num_lives} lives left")
             
     def ask_for_input(self):
+        '''Asks user for input and then validates the input and checks if it is in the chosen word.'''
         guess = input("Please make a guess: ")
         if len(guess) != 1 or not guess.isalpha():
             print("Invalid letter. Please, enter a single alphabetical character.")
@@ -37,6 +41,7 @@ class Hangman:
             self.list_of_guesses.append(guess)
                 
 def play_game(word_list):
+    '''Complete game function which takes a word_list then continues or ends game based on winning or losing conditions'''
     num_lives = 5
     game = Hangman(word_list, num_lives)
     while True:
